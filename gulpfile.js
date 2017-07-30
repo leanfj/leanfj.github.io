0123cf917d9f34db003a5d6094eeef692c4ed73b
leanfj <leanfj@hotmail.com>
2017-07-29 22:58:50 -0300

    Atualização de estrutura de projetos

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -14,7 +17,8 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch("sass/**/*.sass", ['sass']);
-    gulp.watch("./**/*.*").on('change', browserSync.reload);
+    gulp.watch("./js/main.js", ['concatjs']);
+    gulp.watch("./**/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -24,21 +28,55 @@ gulp.task('sass', function() {
         .pipe(autoprefixer({
             browsers: ['Last 2 versions'],
             browsers:['> 2%'],
+            browsers:[
+                      "Android 2.3",
+                      "Android >= 4",
+                      "Chrome >= 20",
+                      "Firefox >= 24",
+                      "Explorer >= 8",
+                      "iOS >= 6",
+                      "Opera >= 12",
+                      "Safari >= 6"
+                    ],
             cascade: false
         }))
         .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
 });
 
+
+//Comprimir CSS
 gulp.task('comprimir', function() {
   return gulp.src("sass/**/*.sass")
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
       .pipe(autoprefixer({
           browsers: ['Last 2 versions'],
           browsers:['> 2%'],
+          browsers:[
+                    "Android 2.3",
+                    "Android >= 4",
+                    "Chrome >= 20",
+                    "Firefox >= 24",
+                    "Explorer >= 8",
+                    "iOS >= 6",
+                    "Opera >= 12",
+                    "Safari >= 6"
+                  ],
           cascade: false
       }))
       .pipe(gulp.dest("./css"))
 });
+
+
+//Concatenar Javascript
+gulp.task('concatjs', function() {
+  return gulp.src(['./node_modules/jquery/dist/jquery.js', './node_modules/bootstrap-sass/assets/javascripts/bootstrap.js', './js/main.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('./js/'))
+    .pipe(rename('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./js/'));
+});
+
 
 gulp.task('default', ['serve']);
