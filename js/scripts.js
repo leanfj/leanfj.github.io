9cfee0435bf47007cb27df122e169a4c6166a0c0
leanfj <leanfj@hotmail.com>
2018-04-10 21:04:52 -0300

    Update wakatime chart ande solve #2

--- js/scripts.js
+++ js/scripts.js
@@ -12644,3 +12644,18 @@ $('[href*="#"]').click(function () {
 	}
 	$(this).addClass('ativo');
 });
+
+
+// wakatime code
+
+var campoWakatime = $('#wakatime');
+
+$.ajax({
+	type: 'GET',
+	url: 'https://wakatime.com/share/@leanfj/c1d85f01-7d19-4c1e-9f5c-99923217beb9.json',
+	dataType: 'jsonp',
+	success: function (response) {
+		console.log(response.data);
+		
+	},
+});
