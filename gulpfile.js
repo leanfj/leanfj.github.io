9c54819b7fad5097f58050edd134e85da12582b4
leanfj <leanfj@hotmail.com>
2018-08-09 21:56:19 -0300

    Portfolio/Site update

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,89 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var concat = require("gulp-concat");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task("serve", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      directory: true
+    }
+  });
 
-    browserSync.init({
-        server: {
-          baseDir: "./",
-          directory: true
-        },
-    });
-
-    gulp.watch("sass/**/*.sass", ['sass']);
-    gulp.watch("./js/main.js", ['concatjs']);
-    gulp.watch("./**/*.html").on('change', browserSync.reload);
+  gulp.watch("sass/**/*.sass", gulp.parallel("sass"));
+  gulp.watch("./js/main.js", gulp.parallel("concatjs"));
+  gulp.watch("./**/*.html").on("change", browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("sass/**/*.sass")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['Last 2 versions'],
-            browsers:['> 2%'],
-            browsers:[
-                      "Android 2.3",
-                      "Android >= 4",
-                      "Chrome >= 20",
-                      "Firefox >= 24",
-                      "Explorer >= 8",
-                      "iOS >= 6",
-                      "Opera >= 12",
-                      "Safari >= 6"
-                    ],
-            cascade: false
-        }))
-        .pipe(gulp.dest("./css"))
-        .pipe(browserSync.stream());
+gulp.task("sass", function() {
+  return gulp
+    .src("sass/**/*.sass")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["Last 2 versions"],
+        browsers: ["> 2%"],
+        browsers: [
+          "Android 2.3",
+          "Android >= 4",
+          "Chrome >= 20",
+          "Firefox >= 24",
+          "Explorer >= 8",
+          "iOS >= 6",
+          "Opera >= 12",
+          "Safari >= 6"
+        ],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
 });
-
 
 //Comprimir CSS
-gulp.task('comprimir', function() {
-  return gulp.src("sass/**/*.sass")
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(autoprefixer({
-          browsers: ['Last 2 versions'],
-          browsers:['> 2%'],
-          browsers:[
-                    "Android 2.3",
-                    "Android >= 4",
-                    "Chrome >= 20",
-                    "Firefox >= 24",
-                    "Explorer >= 8",
-                    "iOS >= 6",
-                    "Opera >= 12",
-                    "Safari >= 6"
-                  ],
-          cascade: false
-      }))
-      .pipe(gulp.dest("./css"))
+gulp.task("comprimir", function() {
+  return gulp
+    .src("sass/**/*.sass")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["Last 2 versions"],
+        browsers: ["> 2%"],
+        browsers: [
+          "Android 2.3",
+          "Android >= 4",
+          "Chrome >= 20",
+          "Firefox >= 24",
+          "Explorer >= 8",
+          "iOS >= 6",
+          "Opera >= 12",
+          "Safari >= 6"
+        ],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("./css"));
 });
-
 
 //Concatenar Javascript
-var jquery = './node_modules/jquery/dist/jquery.js';
-var bootstrap = './node_modules/bootstrap-sass/assets/javascripts/bootstrap.js';
-var mainJS = './js/main.js'
+var jquery = "./node_modules/jquery/dist/jquery.js";
+var bootstrap = "./node_modules/bootstrap-sass/assets/javascripts/bootstrap.js";
+var mainJS = "./js/main.js";
 
-gulp.task('concatjs', function() {
-  return gulp.src([jquery, bootstrap, mainJS])
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./js/'))
-    .pipe(rename('scripts.min.js'))
+gulp.task("concatjs", function() {
+  return gulp
+    .src([jquery, bootstrap, mainJS])
+    .pipe(concat("scripts.js"))
+    .pipe(gulp.dest("./js/"))
+    .pipe(rename("scripts.min.js"))
     .pipe(uglify())
-    .pipe(gulp.dest('./js/'));
+    .pipe(gulp.dest("./js/"));
 });
 
-
-gulp.task('default', ['serve']);
+gulp.task("default", gulp.parallel("serve"));
--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ $('[href*="#"]').click(function () {
 
 // wakatime code
 
-var campoWakatime = $('#wakatime');
 
 $.ajax({
 	type: 'GET',
@@ -24,6 +23,55 @@ $.ajax({
 	dataType: 'jsonp',
 	success: function (response) {
 		console.log(response.data);
-		
+		var ctx = document.getElementById('myChart').getContext('2d');
+		var labels = [];
+		var values = []
+		response.data.forEach(function(element) {
+			console.log(element.name);
+			labels.push(element.name);			
+			values.push(element.percent);			
+		});
+		var chart = new Chart(ctx, {
+			// The type of chart we want to create
+			type: 'doughnut',
+			
+			// The data for our dataset
+			data: {
+				labels: labels,
+				datasets: [{
+					backgroundColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)',
+						'rgba(80, 241, 129, 1)'
+					],
+					borderColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)',
+						'rgba(80, 241, 129, 1)'
+					],
+					data: values,
+				}]
+			},
+			
+			// Configuration options go here
+			options: {
+				legend: {
+					labels: {
+							// This more specific font property overrides the global property
+							fontColor: '#ffffff',
+							defaultFontSize: '14'
+					}
+			}
+			}
+		});
 	},
 });
+
